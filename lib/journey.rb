cac76c9a9122a735cb86058763a807e43779f41a
George Dainton <gcdainton94@gmail.com>
2022-04-27 16:51:15 +0100

    Created Journey class with entry and exit station

---
+++ lib/journey.rb
@@ -0,0 +1,16 @@
+class Journey
+  attr_reader :entry_station, :exit_station
+# def initialize
+#   @entry_station = 
+# end 
+
+def add_entry_station(station)
+  @entry_station = station
+end
+
+def add_exit_station(station)
+  @exit_station = station
+end
+
+
+end 
---
+++ spec/journey_spec.rb
@@ -0,0 +1,30 @@
+require 'journey'
+
+describe Journey do
+
+let(:entry_station){ double :entry_station }
+let(:entry_station){ double :exit_station }
+
+  it 'can create new journeys' do
+    expect(subject).to be_an_instance_of(Journey)
+  end 
+
+  xit 'knows what station it started it\'s journey at' do
+    expect(subject.entry_station).to eq 'Embankment'
+  end 
+
+  it 'responds to add station' do
+    expect(subject).to respond_to(:add_entry_station).with(1).argument 
+  end 
+
+  it 'adds entry station' do
+    expect(subject.add_entry_station(:entry_station)).to eq :entry_station
+  end
+
+  it 'adds exit station' do
+    expect(subject.add_exit_station(:exit_station)).to eq :exit_station
+  end
+
+
+
+end 
