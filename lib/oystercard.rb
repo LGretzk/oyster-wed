252a08bafbd7e9f2bb34f7c624934df7a7f47367
Luiza <luiza@Luizas-MacBook-Air.local>
2022-04-28 18:01:13 +0100

    Adds incomplete journey to journey history

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,14 +4,11 @@ require_relative 'station'
 class Oystercard
 
   attr_reader :balance, :journey, :journey_history
-  
   MAXIMUM_CAPACITY = 90
   MINIMUM_BALANCE = 1
-  FARE = 1
 
   def initialize
     @balance = 0
-    #@journey = {in: nil, out: nil}
     @journey_history = []
   end
 
@@ -22,37 +19,32 @@ class Oystercard
 
   def touch_in(entry_station)
     fail "Not enough funds" if not_enough?
-    if !@journey_history.empty?
-      @journey_history << @new_journey.journey_hash
-    else
-    #fail 'Oyster already touched in' if @new_journey.in_journey?
-    #@journey[:in] = entry_station
-      new_journey
-      @new_journey.add_entry_station(entry_station)
+    ## check if there is a previous journey by checking the journey history array
+    ## check if previous journey is incomplete & if incomplete push to history
+    if !@journey_history.empty? && (@new_journey.journey_hash[:in] != nil && @new_journey.journey_hash[:out] == nil)
+    @journey_history << @new_journey.journey_hash
     end
+    ## charge penalty fare
+
+    #fail 'Oyster already touched in' if @new_journey.in_journey?
+    new_journey
+    @new_journey.add_entry_station(entry_station)
   end
 
   def touch_out(exit_station)
     #fail 'Oyster not touched in' if !in_journey?
-    deduct
     @new_journey.add_exit_station(exit_station)
-    #@journey[:out] = exit_station 
+    deduct
     @journey_history << @new_journey.journey_hash
     #@journey = {in: :nil, out: nil}
   end
 
-  # def in_journey?
-  #   @journey[:in] != nil && @journey[:out] == nil
-  # end
-
   def new_journey
     @new_journey = Journey.new
+    @new_journey
   end
 
   private
-
-
-
 
   def not_enough?
     @balance < MINIMUM_BALANCE
@@ -63,7 +55,7 @@ class Oystercard
   end
 
   def deduct
-    @balance -= FARE
+    @balance -= @new_journey.calculate_fare
   end
 
 end
